0051111358ef222edb606b8b302de51086b44546
Pasumarti.Saiadarsh <Pasumarti.Saiadarsh@DIGTBGL-L6989.godigit.corp>
2023-08-20 11:18:06 +0530

    changes

--- src/main/java/com/digit/test/p1/TestingCode.java
+++ src/main/java/com/digit/test/p1/TestingCode.java
@@ -3,6 +3,6 @@ package com.digit.test.p1;
 public class TestingCode {
 	public static void main(String[] args) {
 		
-		System.out.println("Hi From  Sai Adarsh");
+		System.out.println("Hi From Sai Adarsh");
 	}
 }
